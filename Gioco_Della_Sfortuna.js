6fdd54c752fbf6eb0bf3724ad96ea695a131ae9e
ClaudiaSeri3 <claudiaseri3@gmail.com>
2026-05-15 14:43:19 +0200

    Inserimento prima carta

--- Gioco_Della_Sfortuna.js
+++ Gioco_Della_Sfortuna.js
@@ -9,3 +9,7 @@ import { View, Text, Button, Image, StyleSheet, SafeAreaView } from "react-nativ
 /**
  * Inserimento di 50 carte da gioco con: indice di sfortuna, immagine inerente e il titolo della carta 
  */
+
+const Carte = [
+    { id: 1, nome: "Ti si rompe la racchetta", immagine: "https://www.repstatic.it/content/nazionale/img/2023/05/09/182152591-3fd43fdf-5697-4bd1-90f4-a6346c30fbc5.jpg", indice: 5.0 },
+]
