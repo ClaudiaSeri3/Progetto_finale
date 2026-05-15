5ed0273394b3e29f355974b96af3072b8fabb57e
ClaudiaSeri3 <claudiaseri3@gmail.com>
2026-05-15 14:21:27 +0200

    Inserimento dei primi tag, documentazione e import necessari per il funzionamento del codice

--- Gioco_Della_Sfortuna.js
+++ Gioco_Della_Sfortuna.js
@@ -1,1 +1,11 @@
-cisoa
+/**
+ * @author Claudia Seri
+ * @description Gioco della Sfortuna 
+ */
+
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, Image, StyleSheet, SafeAreaView } from "react-native";
+
+/**
+ * Inserimento di 50 carte da gioco con: indice di sfortuna, immagine inerente e il titolo della carta 
+ */
