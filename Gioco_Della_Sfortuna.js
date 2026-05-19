d101dfce267cc1b904f63a64f6ebd71a21fb0d21
ClaudiaSeri3 <claudiaseri3@gmail.com>
2026-05-19 15:25:36 +0200

    Inserimento dei cambi di stato, immagini e seconda carta

--- Gioco_Della_Sfortuna.js
+++ Gioco_Della_Sfortuna.js
@@ -11,5 +11,40 @@ import { View, Text, Button, Image, StyleSheet, SafeAreaView } from "react-nativ
  */
 
 const Carte = [
-    { id: 1, nome: "Ti si rompe la racchetta", immagine: "https://www.repstatic.it/content/nazionale/img/2023/05/09/182152591-3fd43fdf-5697-4bd1-90f4-a6346c30fbc5.jpg", indice: 5.0 },
-]
+    { id: 1, nome: "Ti si rompe la racchetta", immagine: "1.png"},
+    { id: 2, nome: "Perdi una partita semplice", immagine: "2.png"},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+
+export default function App (){
+    const [fase, setFase] = useState("home");      
+    const [mano, setMano] = useState([]);
+    const [cartaRound, setCartaRound] = useState(null);
+    const [vite, setVite] = useState(3);
+    const [timer, setTimer] = useState(30);
+    const [messaggio, setMessaggio] = useState("");
+}
+
+
+
+
+
+
