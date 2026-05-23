05b2a3cc15cee1721bd6d777a42ea60019f9d13b
ClaudiaSeri3 <claudiaseri3@gmail.com>
2026-05-23 16:47:57 +0200

    Funzione grafica delle vite, aggiunta di altre carte

--- Gioco_Della_Sfortuna.js
+++ Gioco_Della_Sfortuna.js
@@ -27,9 +27,48 @@ const Carte = [
     { id: 14, nome: "Ti dimentichi l'asciugamano", immagine: "https://copilot.microsoft.com/th/id/BCO.e2197822-3ff5-42cc-bcb4-0b98924a4d40.png", indice: 31.0 },
     { id: 15, nome: "Perdi un punto per distrazione", immagine: "https://copilot.microsoft.com/th/id/BCO.90d024d4-7f02-49d3-bc96-25e851300d6c.png", indice: 33.0 },
     { id: 16, nome: "Ti si rompe il laccio", immagine: "https://copilot.microsoft.com/th/id/BCO.5acffb29-6429-4a44-8117-298693c9c467.png", indice: 35.0 },
-     
-    
-    
+    { id: 17, nome: "Si spezza la barretta energetica durante un match", immagine: "https://copilot.microsoft.com/th/id/BCO.ab7c604c-16c9-482b-a619-6660c15e0f99.png", indice: 37.0 }, 
+    { id: 18, nome: "Sbagli un gol", immagine: "https://copilot.microsoft.com/th/id/BCO.0a5dc312-14ef-40c6-8b4f-dc9c4da2f9dc.png", indice: 39.0 },
+    { id: 19, nome: "Inciampi nel tappetino", immagine: "https://copilot.microsoft.com/th/id/BCO.bd96b124-6700-4cd5-a3a2-9a59a9787a7d.png", indice: 41.0 },
+    { id: 20, nome: "Ti si rompe il grip", immagine: "https://copilot.microsoft.com/th/id/BCO.d9227356-8f46-4cc8-9530-dcbb4e71358e.png", indice: 43.0 },
+    { id: 21, nome: "Ti dimentichi la maglia di ricambio", immagine: "https://copilot.microsoft.com/th/id/BCO.e581f928-d445-491c-a692-3682da182123.png", indice: 45.0 },
+    { id: 22, nome: "Ti entra un insetto in faccia", immagine: "https://copilot.microsoft.com/th/id/BCO.65cda21e-705f-48da-a39f-9cf76cb3740f.png", indice: 47.0 },
+    { id: 23, nome: "Sbagli la battuta decisiva", immagine: "https://copilot.microsoft.com/th/id/BCO.d8c536a1-0a57-45f4-b45b-254bdb31bfd5.png", indice: 49.0 },
+    { id: 24, nome: "Ti si rompe il borsone", immagine: "https://copilot.microsoft.com/th/id/BCO.c281515e-0f77-4d3d-9ba1-7e8f7ae3ef2c.png", indice: 51.0 },
+    { id: 25, nome: "Ti si incastra la felpa", immagine: "https://copilot.microsoft.com/th/id/BCO.f5f662c7-de14-4e70-abdc-50b9619e6ab9.png", indice: 53.0 },
+    { id: 26, nome: "Ti cade la racchetta", immagine: "https://copilot.microsoft.com/th/id/BCO.88bc9ae3-ba7a-4062-a713-bdce5bd13fd9.png", indice: 55.0 },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ];
@@ -52,8 +91,21 @@ export default function App (){
     setVite(3);
     setFase("gioco");
   };
+
+  /**
+   * @description Funzione grafica che mostra 3 cuori rappresentanti le vite disponibili, quando si perde una vita il cuore diventa bianco
+   * in caso contrario rimane rosso
+   */
   
-  
+    const Vite = () => {
+    return (
+      <Text style={styles.vite}>
+        {vite >= 1 ? "❤️" : "🤍"}
+        {vite >= 2 ? "❤️" : "🤍"}
+        {vite >= 3 ? "❤️" : "🤍"}
+      </Text>
+    );
+  };
 
 
 
